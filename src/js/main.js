4a6333069331d964c9c6b1209740678bb663f9cc
Vinícius <paulo.vinicius3422@gmail.com>
2024-10-12 04:07:54 -0300

    first part of the carousel created

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,35 @@ const navMenu = document.getElementById("nav-menu"),
     navClose = document.getElementById("nav-close");
 
 function menuShow() {
-    if (navMenu.classList.contains("show-menu")){
+    if (navMenu.classList.contains("show-menu")) {
         navMenu.classList.remove("show-menu");
-    }else {
+    } else {
         navMenu.classList.add("show-menu");
     }
 };
 
 navToggle.onclick = () => menuShow();
 navClose.onclick = () => menuShow();
+
+
+const swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    grabCursor: true,
+    slidesPerView: 'auto',
+
+    // navigation:{
+    //     nextEl:true,
+    //     prevEl:true,
+    // },
+
+    breakpoints: {
+        768: {
+            sliderPerview: 3,
+            centeredSlides: 'auto,'
+        },
+        1150: {
+            centeredSlides: 'auto',
+            spaceBetween: -64,
+        }
+    }
+})
