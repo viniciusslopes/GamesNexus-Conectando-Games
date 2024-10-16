4107295cec494320cd5ceb601eb1fe4c2c550be9
Vinícius <paulo.vinicius3422@gmail.com>
2024-10-15 22:56:11 -0300

    add styles add interaction creat page top list games

--- src/js/main.js
+++ src/js/main.js
@@ -23,12 +23,12 @@ const swiperHome = new Swiper('.home__swiper', {
         delay: 4000,
     },
 
-    effect: 'creative',
+    // effect: 'creative',
     creativeEffect: {
         prev: {
             translate: ['100%', 0, -400],
             opacity: 0,
-            },
+        },
         next: {
             translate: ['100%', 0, 0],
             opacity: 0,
@@ -45,4 +45,43 @@ const swiperHome = new Swiper('.home__swiper', {
             spaceBetween: -64,
         }
     }
-})
+});
+
+const tittleDynamic = document.getElementById("tittle-dynamic");
+const text = "TOP LIST GAMES!";
+const interval = 170;
+
+function showTittleDynamic(tittleDynamic, text, interval) {
+    const charText = text.split("").reverse();
+    const typeText = setInterval(() => {
+        if (!charText.length) {
+            return clearInterval(typeText);
+        };
+
+        const next = charText.pop();
+        tittleDynamic.innerHTML += next;
+    }, interval);
+};
+
+showTittleDynamic(tittleDynamic, text, interval);
+
+
+const backGroundHeader = () => {
+    const header = document.getElementById("header");
+    this.scrollY >= 50 ? header.classList.add("bg-header") : header.classList.remove("bg-header");
+};
+
+window.addEventListener("scroll", backGroundHeader);
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '40px',
+    duration: 2500,
+    reset: true,
+});
+
+
+
+sr.reveal(`.home__swiper`,{delay:200});
+sr.reveal(`.recommendation__list`,{delay:100});
